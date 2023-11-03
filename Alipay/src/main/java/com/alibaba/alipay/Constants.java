24413d458d808518d46b9cf015ab79266ae50c24
zengqy <584371521@qq.com>
2023-11-03 18:57:30 +0800

    测试

--- Alipay/src/main/java/com/alibaba/alipay/Constants.java
+++ Alipay/src/main/java/com/alibaba/alipay/Constants.java
@@ -3,4 +3,5 @@ package com.alibaba.alipay;
 public class Constants {
     public static final String KEY_BILL_INFO = "key_bill_info";
     public static final String KEY_PAY_MONEY = "key_pay_money";
+    
 }
